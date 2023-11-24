d2c17d62bc5cf4e700dd9b847c61d9ebda0ad05b
KhanhLinh <115655284+Khanhlinh952001@users.noreply.github.com>
2023-11-25 01:41:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -79,6 +79,9 @@ io.on('connection', (socket) => {
 });
 
 // Lắng nghe cổng 3000
-server.listen(3000, () => {
-  console.log('Server đang chạy tại http://localhost:3000');
-});
+
+const port = process.env.PORT || 3000;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
